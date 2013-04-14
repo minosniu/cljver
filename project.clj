ff6af06054fee30deeaae3d528718ebf6a68736b
KangWooLee <kangwool@usc.edu>
2013-04-14 01:07:50 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler cljen.handler/app}         
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-core "1.1.6"]
                  [org.clojure/java.jdbc "0.3.0-alpha1"]
