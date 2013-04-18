a2ab0ec3a99b9d4fdfbd3c77c7b276a881dc4176
KangWooLee <kangwool@usc.edu>
2013-04-18 12:18:56 -0700

    Update handler.clj

--- src/cljen/handler.clj
+++ src/cljen/handler.clj
@@ -5,68 +5,70 @@
             [compojure.route :as route]
             [com.ashafa.clutch :as clutch]
             [clojure.data.json :as json]))
-(defstruct JSON :user :project)
-(defn make-JSON [user project] 
-  (struct JSON user project))
 
-;(defstruct USER-JSON :name :password)
-;(defstruct TEMPLATE-JSON :name :in :out)
-;(defstruct DESIGN-JSON :project)
-;(defn make-JSON [{params :params}] 
-;  (case type  
-;   "user" (struct USER-JSON name password)
-;   "template" (struct TEMPLATE-JSON lib-name in out)
-;   "design" (struct DESIGN-JSON project)
-;   "test" (struct JSON user template design type))
-;  )
-
-;usage: (make-JSON user template design)
 
 (def all-design (ref #{}))
-(def test_in {:project "proj1", :user "foo1" :new_block "spindle1"})
-(def test_in1 {:project "proj1", :user "foo2" :new_block "spindle2"})
-(def test_in2 {:project "proj1", :user "foo1" :new_block "spindle2"})
-(def test_in3 {:project "proj2", :user "foo1" :new_block "spindle2"})
-(def test_in4 {:project "proj2", :user "foo3" :new_block "spindle2"})
+; user : (:username :password :type)
+; template : (:libname :in :out :type)
+; design : (:project :username :type)
+(def DB (ref "nerf-db"))
+(def user-db (assoc (cemerick.url/url "http://localhost:5984/" @DB)
+                    :username "admin"
+                    :password "admin"))
+
+(def USER_ref (ref {:name "" :password "" :type "user"}))
+(def TEMPLATE_ref (ref {:name "" :in "" :out "" :type "template"}))
+(def PROJECT_ref (ref {:name "" :project "" :type "project"})) ;will have block info too
+(defn design [user project action-data]
+  (case action-data
+    "new" (dosync (ref-set USER_ref (merge @USER_ref {:name user}))
+            (ref-set PROJECT_ref (merge @PROJECT_ref {:name user :project project}))
+            (if (nil? (clutch/get-document @DB user))
+                        (clutch/put-document @DB (merge @USER_ref {:_id user}))
+                        (println "user exists."))
+            (if (nil? (clutch/get-document @DB (str user "-" project)))
+              (clutch/put-document @DB (merge @PROJECT_ref {:_id (str user "-" project)}))
+              (println "project exists."))
+            )  
+    "save"  (clutch/with-db @DB
+            (-> (clutch/get-document user)
+                (clutch/update-document @USER_ref))
+            (-> (clutch/get-document (str user "-" project))
+                (clutch/update-document @PROJECT_ref))
+            )
+    "load" (dosync (ref-set USER_ref  (clutch/dissoc-meta (clutch/get-document "nerf-db" user)))
+             (ref-set PROJECT_ref (clutch/dissoc-meta (clutch/get-document "nerf-db" (str user "-" project)))))            
+    )
+  ) 
+  
+(defn block [user project action-data]
+  (case action-data
+    "new" ;add one, save also inside project_ref
+    "delete" ;remove one, also delete inside project_Ref
+    "connect" ;change sth
+    "disconnect" ;change sth
+    )
+  )
+; input: {"user" : "ZY", "project" : "proj1" , "action" : {"type" : "project", "data" : "new"}}
 (defn parse_input [request]
   (let [input (json/read-json request)
-        user (keyword (input :user)) 
-        project (keyword (input :project))
-        id {:id (input :new_block)}]
-    (if (nil? (all-design user));true-generate new, false-merge
-      (dosync (println "new user")
-        (ref-set all-design 
-                 (into {} [@all-design
-                           (zipmap [user] [{project [{:id (str (input :new_block) 1)}]}])])))
-              ;(zipmap [(keyword (a :user))] [{(keyword (a :project)) {:id (a :new_block)}}])])))
-      (if (nil? ((all-design user) project)) ;true- add id, false- add proj
-        (dosync (println "user info exists and no project")
-          (ref-set all-design 
-                   (merge @all-design 
-                          {user (into {} [(all-design user) {project [{:id (str (input :new_block) 1)}]}])})))
-        (dosync (println "user and project exist")
-          (ref-set all-design 
-                   (into {} [@all-design 
-                             (zipmap [user]
-                                     [(zipmap [project] 
-                                              [(conj ((all-design user) project) {:id (str (input :new_block)(+ 1 (alength (into-array((all-design user) project)))))})])])])));assemble again     
-        ))))
-;(json/read-str "{\"user\" : \"foo1\", \"project\" : \"proj1\", \"new_block\" : \"spindle1\"}")
-;{"user" : "foo1", "project" : "proj1", "new_block" : "spindle1"}
+        user (input :user)
+        project (input :project)
+        action-type ((input :action) :type)
+        action-data ((input :action) :data)]
+    (case action-type
+      "block" (block user project action-data)
+      "project" (design user project action-data))))
 ;DB part, if DB is not connected, comment these four lines.
-;(def DB (ref "nerf-db"))
-;(def nerf-db (assoc (cemerick.url/url "http://127.0.0.1:5984/" @DB)
-;                    :username "admin"
-;                    :password "admin"))
 (defroutes app-routes
   (GET "/" [] "Welcome!")
   (POST "/" [input] (doseq[] (let [input_str (json/read-json input)]
                                      
                                      (parse_input input)
                                      ;(clutch/with-db nerf-db (clutch/put-document nerf-db @all-design))
-                                     (str @all-design)                             
+                                     ;(str @all-design)                             
                                      ;(str (json/read-json (json/read-json input)))
-                                     ;(str test_in)
+                                     (str @USER_ref @PROJECT_ref)
                                      ;(str input)
                                      )))
   (route/resources "/")
