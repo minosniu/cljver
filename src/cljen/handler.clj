310f6e845cf632a08d375c5a17578a997762ecde
KangWooLee <kangwool@usc.edu>
2013-04-20 03:58:25 -0700

    Update handler.clj

--- src/cljen/handler.clj
+++ src/cljen/handler.clj
@@ -18,7 +18,7 @@
 
 (def USER_ref (ref {:name "" :password "" :type "user"}))
 (def TEMPLATE_ref (ref {:name "" :in "" :out "" :type "template"}))
-(def PROJECT_ref (ref {:user "" :name "" :type "project"})) ;will have block info too
+(def PROJECT_ref (ref {:user "" :name "" :type "project" :data []})) ;will have block info too
 (def ERROR (ref {:result "success" :code 2 :description "" :project_id ""}));result = error -> code., result=success -> ""
 (defn design [user project action-data]
   (case action-data
@@ -38,19 +38,42 @@
             (-> (clutch/get-document (str user "-" project))
                 (clutch/update-document @PROJECT_ref))
             )
-    "load" (dosync (ref-set USER_ref  (clutch/dissoc-meta (clutch/get-document "nerf-db" user)))
-             (ref-set PROJECT_ref (clutch/dissoc-meta (clutch/get-document "nerf-db" (str user "-" project)))))            
-    )
-  ) 
+    "load" (dosync (ref-set USER_ref  (clutch/dissoc-meta (clutch/get-document @DB user)))
+             (ref-set PROJECT_ref (clutch/dissoc-meta (clutch/get-document @DB (str user "-" project)))))            
+    )) 
+(defn connect-block [id-list]
+(let [in (first id-list) 
+      out (second id-list)] 
+  (merge 
+  (dosync (first (filter #(= (:name %) (first in)) (PROJECT_ref :data)))))
+))  
+(defn disconnect-block [id-list]
+(let [in (first id-list)
+      out (second id-list)]
   
+)) 
 (defn block [user project action-data]
-  (case action-data
-    "new" ;add one, save also inside project_ref
-    "delete" ;remove one, also delete inside project_Ref
-    "connect" ;change sth, input&output
-    "disconnect" ;change sth, output
-    )
-  )
+  (let [KEY (first (keys action-data))
+        VAL (action-data KEY)
+        block_count (alength (into-array (PROJECT_ref :data)))
+        alloc_count (if (= block_count 0) 
+                      (+ 1 block_count)
+                      (+ 1 (Integer. (re-find #"\d+" ((last (PROJECT_ref :data)) :name)))))
+        block_data (PROJECT_ref :data)]
+  (case KEY
+    :new (dosync (ref-set TEMPLATE_ref  (clutch/dissoc-meta (clutch/get-document @DB VAL)))
+           (ref-set TEMPLATE_ref (merge @TEMPLATE_ref {:name (str (TEMPLATE_ref :name) alloc_count)}))
+           (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) @TEMPLATE_ref)})))
+    ;find block from library and save in memory
+    :delete (dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data  
+              (into [] (filter #(not= (:name %) VAL) (PROJECT_ref :data)))})))
+    ;remove one block from memory
+    :connect ( ;input 
+               ;output change
+               ;ref-set
+               )
+    :disconnect ;change sth, output
+    )))
 ; input: {"user" : "ZY", "project" : "proj1" , "action" : {"type" : "project", "data" : "new"}}
 (defn parse_input [request]
   (let [input (json/read-json request)
@@ -65,13 +88,13 @@
 (defroutes app-routes
   (GET "/" [] "Welcome!")
   (POST "/" [input] (doseq[] (let [input_str (json/read-json input)]
-                                     
+                                     "error msg, block&design _id response"
                                      (parse_input input)
                                      ;(clutch/with-db nerf-db (clutch/put-document nerf-db @all-design))
                                      ;(str @all-design)                             
                                      ;(str (json/read-json (json/read-json input)))
-                                     (json/write-str @ERROR)
-                                     ;(json/write-json @ERROR); -> error response in JSON
+                                     ;(json/write-str @ERROR)
+                                     (str @PROJECT_ref (alength (into-array (PROJECT_ref :data))))
                                      ;(str input)
                                      ))) 
   (route/resources "/")
