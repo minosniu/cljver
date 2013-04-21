dc82aadd26322cc55f62f853ac9ab71dab9cd5f3
KangWooLee <kangwool@usc.edu>
2013-04-21 04:53:01 -0700

    Update handler.clj

--- src/cljen/handler.clj
+++ src/cljen/handler.clj
@@ -20,8 +20,11 @@
 (def TEMPLATE_ref (ref {:name "" :in "" :out "" :type "template"}))
 (def PROJECT_ref (ref {:user "" :name "" :type "project" :data {}})) ;will have block info too
 (def ERROR (ref {:result "success" :code 2 :description "" :project_id ""}));result = error -> code., result=success -> ""
-(defn design [user project action-data]
-  (case action-data
+(def OUTPUT (ref ""))
+
+(defn design [user project action]
+  ;{"user" : "ZY", "project" : "proj21" , "action" : "new", "type" : "project"}
+  (case action
     "new" (dosync (ref-set USER_ref (merge @USER_ref {:name user}))
             (ref-set PROJECT_ref (merge @PROJECT_ref {:user user :name project}))
             (if (nil? (clutch/get-document @DB user))
@@ -30,88 +33,101 @@
             (if (nil? (clutch/get-document @DB (str user "-" project)))
               (when (= (ERROR :code) "") (clutch/put-document @DB (merge @PROJECT_ref {:_id (str user "-" project)})))
               (when-not (= 1 (ERROR :code)) (ref-set ERROR (merge @ERROR {:result "error" :code 2 :description "the project already exists" :project_id ""})))) ;-> no user error, project exist error, success
-            )  
+            (ref-set OUTPUT (conj @ERROR {:project_id (str user "-" project)})))  
                   ;(clutch/put-document @DB (merge @USER_ref {:_id user})); don't create user! don't use it
-    "save"  (clutch/with-db @DB
-            (-> (clutch/get-document user)
-                (clutch/update-document @USER_ref))
-            (-> (clutch/get-document (str user "-" project))
-                (clutch/update-document @PROJECT_ref))
-            )
+    "save"  (dosync (clutch/with-db @DB
+                      (-> (clutch/get-document user)
+                        (clutch/update-document @USER_ref))
+                      (-> (clutch/get-document (str user "-" project))
+                        (clutch/update-document @PROJECT_ref))
+                      (ref-set OUTPUT {:result "success" :code "" :description ""})))
     "load" (dosync (ref-set USER_ref  (clutch/dissoc-meta (clutch/get-document @DB user)))
-             (ref-set PROJECT_ref (clutch/dissoc-meta (clutch/get-document @DB (str user "-" project)))))            
-    )) 
-(defn connect-block [id-list]
-(let [in (first id-list) 
-      out (second id-list)
-      in_key (keyword in)
-      out_key (keyword out)] 
-  ;{:data (conj {:spindle1 (merge ((PROJECT_ref :data) :spindle1) {:in 3})} (PROJECT_ref :data))}
-  (dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {in_key (merge ((PROJECT_ref :data) in_key) {:out out})} )}))
-    (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {out_key (merge ((PROJECT_ref :data) out_key) {:in in})} )})))
-;(dosync  (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj {in_key (merge ((PROJECT_ref :data) in_key) {:out in})} (PROJECT_ref :data))})))
-  ;(dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data ""   })))
-  ;(dosync (first (filter #(= (:name %) (first in)) (PROJECT_ref :data))))
-))   
-(defn disconnect-block [id-list]
-(let [in (first id-list)
-      out (second id-list)
-      in_key (keyword in) 
-      out_key (keyword out)]
-   (dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {in_key (merge ((PROJECT_ref :data) in_key) {:out ""})} )}))
-    (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {out_key (merge ((PROJECT_ref :data) out_key) {:in ""})} )})))
-  
-)) 
-(defn block [user project action-data]
-  (let [KEY (first (keys action-data))
-        VAL (action-data KEY)
-        block_count (alength (into-array (PROJECT_ref :data)))
+             (ref-set PROJECT_ref (clutch/dissoc-meta (clutch/get-document @DB (str user "-" project))))
+             (ref-set OUTPUT {:result "success" :code "" :description ""})))) 
+
+(defn new-block [data alloc_count]
+  ;{"user" : "ZY", "project" : "proj21" , "action" : "new", "type" : "block", "data": {"template": "spindle", "position": {"left": 20, "top": 30}}}
+  (if (clutch/document-exists? @DB (data :template))
+    (dosync 
+      (ref-set TEMPLATE_ref  (clutch/dissoc-meta (clutch/get-document @DB (data :template))))
+      (ref-set TEMPLATE_ref (merge @TEMPLATE_ref {:name (str (TEMPLATE_ref :name) alloc_count)}))
+      (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {(keyword (TEMPLATE_ref :name)) {:in (TEMPLATE_ref :in) :out (TEMPLATE_ref :out) :position (data :position)}})}))
+      (ref-set OUTPUT {:result "success" :block_id (TEMPLATE_ref :name)}))
+    (dosync (ref-set OUTPUT {:result "error" :code 3 :description "the template does not exist"}))))
+(defn delete-block [data]
+  ;{"user" : "ZY", "project" : "proj21" , "action" : "delete", "type" : "block", "data": {"block": "spindle4"}}
+  (if (nil? ((PROJECT_ref :data)(keyword (data :block)))) 
+    (dosync(ref-set OUTPUT {:result "error" :code 4 :description "the block does not exist" }))
+    (dosync (ref-set OUTPUT {:result "success"})
+      (ref-set PROJECT_ref (merge @PROJECT_ref 
+                                  {:data (apply dissoc (PROJECT_ref :data) [(keyword (data :block))])})))))
+(defn move-block [data]
+  ;{"user" : "ZY", "project" : "proj21" , "action" : "move", "type" : "block", "data":  {"block": "spindle1", "position": {"left": 33, "top": 21}}}
+  (let [block_id (keyword (data :block))]
+    (dosync (if (nil? ((PROJECT_ref :data) block_id)) 
+              (ref-set OUTPUT {:result "error" :code 4 :description "the block does not exist" })
+              (ref-set OUTPUT {:result "success"}))
+      (ref-set PROJECT_ref (merge @PROJECT_ref 
+                                  {:data (conj (PROJECT_ref :data) {block_id (merge ((PROJECT_ref :data) block_id) {:position (data :position)})})})))))
+(defn connect-block [data]
+ ;{"user" : "ZY", "project" : "proj21" , "action" : "connect", "type" : "block", "data":  {"output":{"id": "spindle1", "pin": "out"}, "input":{"id": "spindle2", "pin": "in"}}}
+  (let [out (data :output)
+        in (data :input)
+        out_key (keyword (out :id))
+        in_key (keyword (in :id))
+        out_pin (keyword (out :pin))
+        in_pin (keyword (in :pin))] 
+    (dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {in_key (merge ((PROJECT_ref :data) in_key) {in_pin (out :id)} )} )}))
+      (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {out_key (merge ((PROJECT_ref :data) out_key) {out_pin (in :id)})} )}))
+      (ref-set OUTPUT {:result "success"}))))   
+(defn disconnect-block [data]
+  ;{"user" : "ZY", "project" : "proj21" , "action" : "disconnect", "type" : "block", "data":  {"output":{"id": "spindle1", "pin": "out"}, "input":{"id": "spindle2", "pin": "in"}}}
+  (let [out (data :output)
+        in (data :input)
+        out_key (keyword (out :id))
+        in_key (keyword (in :id))
+        out_pin (keyword (out :pin))
+        in_pin (keyword (in :pin))] 
+    (dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {in_key (merge ((PROJECT_ref :data) in_key) {in_pin ""} )} )}))
+      (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {out_key (merge ((PROJECT_ref :data) out_key) {out_pin ""})} )}))
+      (ref-set OUTPUT {:result "success"}))))   
+
+(defn block [user project action data]
+  (let [block_count (alength (into-array (PROJECT_ref :data)))
+        block_data (PROJECT_ref :data)
         alloc_count (if (= block_count 0) 
                       (+ 1 block_count)
-                      (+ 1 (Integer. (re-find #"\d+" (name (first (keys (PROJECT_ref :data))))))))
-                      ;(+ 1 (Integer. (re-find #"\d+" ((last (PROJECT_ref :data)) :name)))))
-        block_data (PROJECT_ref :data)]
-  (case KEY
-    :new (dosync (ref-set TEMPLATE_ref  (clutch/dissoc-meta (clutch/get-document @DB VAL)))
-           (ref-set TEMPLATE_ref (merge @TEMPLATE_ref {:name (str (TEMPLATE_ref :name) alloc_count)}))
-           (ref-set PROJECT_ref (merge @PROJECT_ref {:data (conj (PROJECT_ref :data) {(keyword (TEMPLATE_ref :name)) {:in (TEMPLATE_ref :in) :out (TEMPLATE_ref :out)}})})))
-    ;find block from library and save in memory
-    :delete (dosync (ref-set PROJECT_ref (merge @PROJECT_ref {:data  
-              ;(into {} (filter #(not= (:name %) VAL) (PROJECT_ref :data)))})))
-               (apply dissoc (PROJECT_ref :data) [(keyword VAL)])})))                                              
-    ;remove one block from memory
-    :connect (connect-block VAL)
-    :disconnect (disconnect-block VAL);change sth, output
-    )))
-; input: {"user" : "ZY", "project" : "proj1" , "action" : {"type" : "project", "data" : "new"}}
+                      (+ 1 (Integer. (re-find #"\d+" (name (first (keys block_data)))))))]
+    (case action
+      "new" (new-block data alloc_count) ;find block from library and save in project in memory
+      "delete"(delete-block data) ;remove one block from project in memory
+      "connect" (connect-block data);change two block pin info in project in memory
+      "disconnect" (disconnect-block data);change two block pin info in project in memory
+      "move" (move-block data) ; change block position in project in memory 
+      )))
 (defn parse_input [request]
   (let [input (json/read-json request)
         user (input :user)
         project (input :project)
-        action-type ((input :action) :type)
-        action-data ((input :action) :data)]
-    (case action-type
-      "block" (block user project action-data)
-      "project" (design user project action-data))))
+        action (input :action)
+        type (input :type)
+        data (input :data)
+        ]
+    (case type
+      "block"  (block user project action data)
+      "project" (design user project action))))
+
 ;DB part, if DB is not connected, comment these four lines.
 (defroutes app-routes
   (GET "/" [] "Welcome!")
   (POST "/" [input] (doseq[] (let [input_str (json/read-json input)]
-                                     "error msg, block&design _id response"
-                                     (parse_input input)
-                                     ;(clutch/with-db nerf-db (clutch/put-document nerf-db @all-design))
-                                     ;(str @all-design)                             
-                                     ;(str (json/read-json (json/read-json input)))
-                                     ;(json/write-str @ERROR)
-                                     (str @PROJECT_ref (alength (into-array (PROJECT_ref :data))))
-                                     ;(str input)
-                                     ))) 
+                               "error msg, block&design _id response"                         
+                               (parse_input input)
+                               ;(str @PROJECT_ref "\n" @OUTPUT)
+                               (json/write-str @OUTPUT)
+                               ))) 
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
-
-
-
-;(json/write-str @all-design)
