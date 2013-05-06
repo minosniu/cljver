3b5a5886801f2e97e3441b0d8bb628f2350b8995
KangWooLee <kangwool@usc.edu>
2013-05-05 19:02:34 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -9,13 +9,10 @@
                  [ring/ring-core "1.1.6"]
                  [org.clojure/java.jdbc "0.3.0-alpha1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 ;[http.async.client "0.5.2"];client
-                 ;[clj-http "0.7.0"] ;client
-                 ;[http-kit "2.0.0"] 
-                 ;[ring/ring-jetty-adapter "1.1.6"]
                  [com.ashafa/clutch "0.4.0-RC1"];clutch (or 0.3.1)
                  [clj-http "0.5.5"];clutch
                  [com.cemerick/url "0.0.6"];clutch
                  [noir "1.3.0"]
                  [org.clojure/data.json "0.2.1"]  
+                 [org.clojure/core.incubator "0.1.2"] 
                    ]);
